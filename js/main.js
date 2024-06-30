0d1d2cd7a4f87cb2a5b9f57564b4fe0ba5f6728f
KyawZinAungOway <kyawzinaung@owaytrip.com>
2024-06-30 09:08:30 +0630

    Update Design

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 50, 'easeInOutExpo');
         return false;
     });
 
@@ -77,8 +77,8 @@
         loop: true,
         nav : true,
         navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
+            '<i class="fas fa-chevron-left"></i>',
+            '<i class="fas fa-chevron-right"></i>'
         ]
     });
 
